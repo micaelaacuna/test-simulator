4282e101e5b5d0636f06860d355be7c2a85c9768
panconmantek <micahl1981@gmail.com>
2024-12-31 00:38:58 -0300

    update questions

--- testing.js
+++ testing.js
@@ -1,19 +1,18 @@
-const fs = require('fs');
+const fs = require("fs");
 
 function countJsonElements(filePath) {
-    fs.readFile(filePath, 'utf8', (err, data) => {
-        if (err) {
-            console.error('Error reading file:', err);
-            return;
-        }
-        try {
-            const jsonData = JSON.parse(data);
-            console.log(`We have ${jsonData.length} questions in the pool!`);
-        } catch (parseErr) {
-            console.error('Error parsing JSON:', parseErr);
-        }
-    });
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      console.error("Error reading file:", err);
+      return;
+    }
+    try {
+      const jsonData = JSON.parse(data);
+      console.log(`We have ${jsonData.length} questions in the pool!`);
+    } catch (parseErr) {
+      console.error("Error parsing JSON:", parseErr);
+    }
+  });
 }
 
-countJsonElements('questions.json');
-
+countJsonElements("questions.json");
